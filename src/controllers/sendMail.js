845c012b758a2f0a2ae10fad32bf02a624926f21
Luz Scarpati <luz.scarpati@gmail.com>
2024-06-28 17:10:31 -0300

    Corrección de incio

--- src/controllers/sendMail.js
+++ src/controllers/sendMail.js
@@ -1,17 +1,11 @@
 import { sendMailservice } from '../services/sendMailservices.js';
-import swal from 'sweetalert';
 
 export const sendMail = async (req, res) => {
     try{
         const { firstName, lastName, email } = req.body;
         const exito = await sendMailservice(firstName, lastName, email);
         if (exito) {
-          swal({
-            title: "Good job!",
-            text: "You clicked the button!",
-            icon: "success",
-          })
-          //res.send('Formulario enviado correctamente');
+         res.send('Formulario enviado correctamente');
         } else {
           res.status(500).send('Error al enviar el formulario');
         }    
