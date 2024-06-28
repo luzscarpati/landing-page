14b02f5bf64998e5f05fb50ba1c6036ee69aabec
Luz Scarpati <luz.scarpati@gmail.com>
2024-06-28 17:38:26 -0300

    Corecciones de ruteo

--- server.js
+++ server.js
@@ -2,13 +2,25 @@ import express from "express";
 import emailRouter from './src/routes/emailRouter.js';
 import config from './src/config/config.js';
 import bodyParser from "body-parser";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.static(__dirname));
+
 app.use('/api', emailRouter);
 
-const PORT  = config.PORT;
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = config.PORT;
 
 app.listen(PORT, () => console.log(`Server UP ON PORT ${PORT}`));
